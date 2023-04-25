bed537c1eeaf35a09ce994f4a0d2e59288c9519b
hullaah <umaradelo1.247@gmail.com>
2023-04-25 19:24:22 +0100

    made some optimization to task 8

--- flags.c
+++ flags.c
@@ -10,12 +10,14 @@ int pound_plus_flag(char *str, char flag, char buf[BUFFER], int *start_string, i
 
 			if (
                                 str[i] == 's' || str[i] == 'p' || str[i] == 'R' || str[i] == 'i'
-                                || str[i] == 'd' || str[i] == 'r' || str[i] == 'c' || str[i] == 's'
-                                || str[i] == 'u' || str[i] == '%'
+                                || str[i] == 'd' || str[i] == 'r' || str[i] == 'c' || str[i] == 'S'
+                                || str[i] == 'u' || str[i] == '%' || str[i] == 'b'
                         )
                                 return (start_buf);
 			i++;
 		}
+                if (str[i] == '\0')
+			return (start_buf);
 		if (str[i] == 'x' || str[i] == 'X')
 		{
 			buf[start_buf] = '0';
@@ -28,7 +30,15 @@ int pound_plus_flag(char *str, char flag, char buf[BUFFER], int *start_string, i
 	else if (flag == '+')
 	{
 		while (str[i] != 'd' && str[i] != '\0')
+                {
+                        if (
+                                str[i] == 's' || str[i] == 'p' || str[i] == 'R' || str[i] == 'x'
+                                || str[i] == 'X' || str[i] == 'r' || str[i] == 'c' || str[i] == 'S'
+                                || str[i] == 'u' || str[i] == '%' || str[i] == 'b' || str[i] == 'o'
+                        )
+                                return (start_buf);
 			i++;
+                }
 		if (str[i] == '\0')
 			return (start_buf);
 		if (str[i] == 'd')
