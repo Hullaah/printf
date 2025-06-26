8dfe5c745517377097476d49399a1b4ab51ec4a8
Umar Adelowo <umaradelo1.247@gmail.com>
2025-06-26 17:51:18 +0100

    Working on the decimal format specifier handler
    Implemented the write_int function which writes an integer in ascii format to the buffer

--- handle_decimal.c
+++ handle_decimal.c
@@ -1,4 +1,45 @@
 #include "printf.h"
+#include <stdarg.h>
+#include <limits.h>
+/**
+ * write_int - Writes an integer to the buffer
+ * @num: The integer to write
+ * @buf: Buffer where the integer will be written
+ * @len: Pointer to an integer that keeps track of the current length of the
+ * buffer
+ *
+ * Description: This function writes the digits of the integer in
+ * to the buffer, flushing it when necessary.
+ */
+static void write_int(int num, char buf[], int *len)
+{
+	int start = *len;
+	int is_int_min = num == INT_MIN;
+
+	if (num < 0)
+	{
+		if (*len == LENGTH)
+		{
+			flush(buf, *len);
+			*len = 0;
+		}
+		buf[(*len)++] = '-';
+		start = *len;
+		num = is_int_min ? INT_MAX : -num;
+	}
+	do
+	{
+		if (*len == LENGTH)
+		{
+			flush(buf, *len);
+			*len = 0;
+		}
+		buf[(*len)++] = '0' + (is_int_min ? num % 10 + 1 : num % 10);
+		num /= 10;
+	} while (num > 0);
+	reverse(buf, start, *len - 1);
+}
+
 /**
  * handle_decimal - Handles the decimal format specifier
  * @fs: Pointer to the FormatSpecifier structure
