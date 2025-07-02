5c47ecd80b922ba2b8c43ef3f28cc72cab977442
Umar Adelowo <umaradelo1.247@gmail.com>
2025-07-02 17:35:27 +0100

    Implemented the percent handler

--- handle_percent.c
+++ handle_percent.c
@@ -3,4 +3,12 @@
 void handle_percent(struct FormatSpecifier *fs, va_list *args, char *buf,
 		    int *len)
 {
+	UNUSED(fs);
+	UNUSED(args);
+	if (*len == LENGTH)
+	{
+		flush(buf, *len);
+		*len = 0;
+	}
+	buf[(*len)++] = '%';
 }
